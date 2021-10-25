347f9af7934587187336b85e79da2d5e49671bcd
venskiy <venskiy@gmail.com>
2021-10-25 15:32:29 +0300

    First lecture commit

---
+++ Examples001_HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examples001_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Examples_003_sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Examples_003_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = new Random().Next(1,10);
+            int nubmerB = new Random().Next(1,10);
+            int result = numberA + nubmerB;
+            Console.Write(result);
+        }
+    }
+}
